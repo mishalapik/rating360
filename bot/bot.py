0f86a5bf4309d9dc4854dcd33df327db5db753e1
Mikhail <lyttma163@gmail.com>
2024-05-03 19:11:47 +0300

    bot created

---
+++ bot/bot.py
@@ -0,0 +1,34 @@
+from aiogram import Bot,Dispatcher,F
+from aiogram.types import Message
+import asyncio
+import logging
+
+token = '7097427541:AAHw6BzeEZFjqOq4jQYLLybCut-_3MrL4sM'
+admin_id='1346629196'
+
+async def bot_started(bot:Bot):
+    await bot.send_message(chat_id=admin_id,text = 'bot started')
+    # await bot.send_message(chat_id=operator, text='bot started')
+async def bot_closed(bot:Bot):
+    await bot.send_message(chat_id=admin_id,text = 'bot closed')
+    # await bot.send_message(chat_id=operator, text='bot closed')
+
+
+
+
+async def start():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=token, parse_mode='HTML')
+    dp = Dispatcher()
+    # dp.message.register(main_menu,F.text == '/menu')
+    
+    dp.startup.register(bot_started)
+    dp.shutdown.register(bot_closed)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+if __name__ == '__main__':
+    asyncio.run(start())
